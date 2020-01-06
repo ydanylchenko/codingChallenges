77a730ee09d14a7bbc2654141645ec96af0fe619
dnische <danilchenkoy@gmail.com>
2020-01-06 17:13:21 -0500

    Add LongestEvenLengthWord.java

---
+++ hackerrank/src/test/java/LongestEvenLengthWord.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LongestEvenLengthWord {
+    public static String longestEvenLengthWord(String text) {
+        return Arrays.stream(text.split(" ")).filter(word -> word.length() % 2 == 0).max(Comparator.comparingInt(String::length)).get();
+    }
+
+    @Test
+    public void lengthTest(){
+        String text = "Time to write great code";
+        System.out.println(longestEvenLengthWord(text));
+    }
+}
